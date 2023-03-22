cdabadd551054f96918183bdcc6be43c862af8fc
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2023-03-22 19:37:48 +0100

    new files

--- WordGame/ViewController.swift
+++ WordGame/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let segueIdGoToGame = "goToGame"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: segueIdGoToGame, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueIdGoToGame {
+            let destinationVC = segue.destination as? GameViewController
+        }
+    }
+    
+    
 
 }
 
